d10704ac697323c9366341d6c5a1a792b9753156
David Golden <dagolden@cpan.org>
2010-01-17 22:25:54 -0500

    remove urn:uuid: notation

--- lib/CPAN/Testers/Common/Utils.pm
+++ lib/CPAN/Testers/Common/Utils.pm
@@ -7,7 +7,6 @@
 package CPAN::Testers::Common::Utils;
 use strict;
 use warnings;
-use URI::urn::uuid qw();
 
 our $VERSION = '0.001';
 $VERSION = eval $VERSION; ## no critic
@@ -31,21 +30,18 @@ our %EXPORT_TAGS = (
 #   NameSpace_URL, "http://nntp.x.perl.org/group/perl.cpan.testers"
 # );
 
-my $base_guid = "ED372D00-B19F-3F77-B713-D32BBA55D77F";
+# Lower case is canonical
+my $base_guid = "ed372d00-b19f-3f77-b713-d32bba55d77f";
 
 sub nntp_to_guid {
   my ($nntp_id) = @_;
   my $guid = $base_guid;
   substr($guid, 0, 8, sprintf("%08d",$nntp_id)); # zero padded
-  my $uri = URI->new('urn:uuid:');
-  $uri->uuid(lc $guid);
-  return $uri->as_string;
+  return $guid;
 }
 
 sub guid_to_nntp {
-  my ($uri_string) = @_;
-  my $uri = URI->new($uri_string);
-  my $guid = $uri->uuid;
+  my ($guid) = @_;
   my ($nntp_id) = $guid =~ m{\A0*([0-9]{1,7})}; # strip leading zeros
   return $nntp_id;
 }
@@ -81,15 +77,15 @@ modules
 
 == Mapping NNTP IDs to GUIDs
 
-Legacy CPAN Testers reports were sent via email and made available via an NNTP
-group, C<perl.cpan.testers>.  Reports were 'indexed' by their NNTP ID.  The next
-generation of CPAN Testers uses a GUID URN to identify reports.
+Legacy CPAN Testers reports were sent via email and made available via an
+NNTP group, C<perl.cpan.testers>.  Reports were 'indexed' by their NNTP ID.
+The next generation of CPAN Testers uses a GUID URN to identify reports.
 
-Old reports with an NNTP ID are mapped to GUIDs by replacing the first 8 hex
-characters of a common 'base GUID' with a zero-padded decimal representation of
-the NNTP ID.
+Old reports with an NNTP ID are mapped to GUIDs by replacing the first 8
+hex characters of a common 'base GUID' with a zero-padded decimal
+representation of the NNTP ID.
 
-  urn:uuid:XXXXXXXX-b19f-3f77-b713-d32bba55d77f
+  XXXXXXXX-b19f-3f77-b713-d32bba55d77f
 
 Such GUID URNs are visually distinctive and have the nice feature of
 sorting earlier than second-generated report GUIDs based on a timestamp.
@@ -100,22 +96,22 @@ Two translation functions are provided for convenience.
 
     $guid    = nntp_to_guid( $nntp_id );
 
-Given a numeric NNTP ID, returns a standard string-form GUID URN.  (No range
+Given a numeric NNTP ID, returns a standard string-form GUID.  (No range
 checking is done.) Examples:
 
-  nntp_to_guid( 51432   );  # urn:uuid:00051432-b19f-3f77-b713-d32bba55d77f
-  nntp_to_guid( 6171265 );  # urn:uuid:06171265-b19f-3f77-b713-d32bba55d77f
+  nntp_to_guid( 51432   );  # 00051432-b19f-3f77-b713-d32bba55d77f
+  nntp_to_guid( 6171265 );  # 06171265-b19f-3f77-b713-d32bba55d77f
 
 === {guid_to_nntp}
 
     $guid    = nntp_to_guid( $nntp_id );
 
-Given a GUID URN of the form described above, returns the decimal number
+Given a GUID string of the form described above, returns the decimal number
 in the first 8 characaters.  (Again, there is no error checking that
-the GUID URN is properly formatted.)  Examples:
+the GUID is properly formatted.)  Examples:
 
-  guid_to_nntp( 'urn:uuid:00051432-b19f-3f77-b713-d32bba55d77f' ); # 51432
-  guid_to_nntp( 'urn:uuid:06171265-b19f-3f77-b713-d32bba55d77f' ); # 6171265
+  guid_to_nntp( '00051432-b19f-3f77-b713-d32bba55d77f' ); # 51432
+  guid_to_nntp( '06171265-b19f-3f77-b713-d32bba55d77f' ); # 6171265
 
 = BUGS
 
@@ -127,7 +123,7 @@ existing test-file that illustrates the bug or desired feature.
 
 = SEE ALSO
 
-* Data::UUID::LibUUID
+* Data::GUID::Any
 
 = AUTHOR
 
